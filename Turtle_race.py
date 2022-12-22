58c6448cc00e3d04867187d33d25c30ea333c9fe
jendakolda <email@jendakolda.cz>
2022-12-22 12:07:12 +0100

    Refactoring ideas 1

--- Turtle_race.py
+++ Turtle_race.py
@@ -1,39 +1,47 @@
-from turtle import Turtle, Screen
+# Klasicka struktura:
+# 1) nejdriv importy
+# 2) pak funkce
+# 3) nakonec main(), kde funkce pouzivam
+
 import random
+from turtle import Turtle, Screen
 
-screen = Screen()
-screen.setup(width=500, height=400)
-user_bet = screen.textinput(title="Make your bet.", prompt="Which turtle will win the race? Enter a colour: ")
-print(user_bet)
 
-colours = ["red", "orange", "yellow", "green", "blue", "purple"]
-y_coordinates_list = range(-170, 238, 68)
+def main():
+    colours = ["red", "orange", "yellow", "green", "blue", "purple"]
+    y_coordinates_list = range(-170, 238, 68)
+    start_position = -220
 
-all_turtles = []
-for new_turtle in colours:
-    new_turtle = Turtle(shape="turtle")
-    all_turtles.append(new_turtle)
+    screen = Screen()
+    screen.setup(width=500, height=400)
+    while True:  # tady je cyklus, kterej bezi, dokud uzivatel nezada existujici barvu
+        user_bet = screen.textinput(title="Make your bet.", prompt="Which turtle will win the race? Enter a colour: ")
+        if user_bet in colours:
+            break
+    print(user_bet)
 
-def define_turtle(colour, y_coordinates):
-    name.color(colour)
-    name.penup()
-    name.goto(-220, y_coordinates)
+    # vhodnejsi struktura je tady slovnik
+    all_turtles = {colour: (Turtle(shape='turtle'), y_coord) for colour, y_coord in zip(colours, y_coordinates_list)}
+    # udelal jsem slovnik, kde key je barva zelvy a value je tuple obsahujici ten objekt Turtle a jeho y souradnici
+    # funkce zip bere postupne dve polozky z nichz jedna je z listu colors a druha z y_coordinates_list. Zip se hodi kdyz iterujes pres vice iterables
+    for colour, (turtle_obj, y_coord) in all_turtles.items():
+        #tohle mozna neni nutny schovavat do funkce: proste vezmes ten objekt Turtle a postupne pouzijes ty metody color, penup, goto
+        turtle_obj.color(colour)
+        turtle_obj.penup()
+        turtle_obj.goto(start_position, y_coord) # tady je vhodnejsi dat promennou misto konkterni hodnoty. Hodnotu promenne si potom definujes hned na zacatku main(), potom je jednodussi se v tom vyznat
 
-for i in range(len(colours)):
-    name = all_turtles[i]
-    define_turtle(colour=colours[i], y_coordinates=y_coordinates_list[i])
+    game_on = True
+    while game_on:
+        for turtle, _ in all_turtles.values():
+            if turtle.xcor() > - start_position:
+                game_on = False
+                winning_colour = turtle.pencolor()
+            turtle.forward(random.randint(0, 10))
 
-game_on = True
-while game_on:
-    for turtle in all_turtles:
-        if turtle.xcor() > 220:
-            game_on = False
-            winning_colour = turtle.pencolor()
-        turtle.forward(random.randint(0, 10))
+    # ten print se da naplacnout na jeden radek, ale u tehle dlouhejch to neni asi moc citelny
+    print(f"The {winning_colour} turtle has won the race. You made the right bet." if  winning_colour.lower() == user_bet.lower() else f"The {winning_colour} turtle has won the race. You made the wrong bet.")
+    screen.exitonclick()
 
-if winning_colour.lower() == user_bet.lower():
-    print(f"The {winning_colour} turtle has won the race. You made the right bet.")
-else:
-    print(f"The {winning_colour} turtle has won the race. You made the wrong bet.")
 
-screen.exitonclick()
+if __name__ == "__main__":
+    main()
